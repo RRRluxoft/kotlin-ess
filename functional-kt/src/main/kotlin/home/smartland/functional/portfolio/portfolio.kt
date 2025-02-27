08ef7860c447e004aa8c5d5a001d561c335591a8
Roman <rrrluxoft@users.noreply.github.com>
2025-02-27 10:46:23 +0100

    functional-kt init

---
+++ functional-kt/src/main/kotlin/home/smartland/functional/portfolio/portfolio.kt
@@ -0,0 +1,129 @@
+package home.smartland.functional.portfolio
+
+import arrow.core.nonFatalOrThrow
+import arrow.core.raise.Raise
+import arrow.core.raise.RaiseDSL
+import arrow.core.raise.fold
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.coroutines.cancellation.CancellationException
+import kotlin.experimental.ExperimentalTypeInference
+
+
+//interface CreatePortfolioUseCase {
+//    fun Raise<DomainError>.createPortfolio(model: CreatePortfolio): PortfolioId
+//}
+
+
+interface Jobs {
+    fun Raise<JobError>.getById(id: JobId): Job
+
+    context (Logger, Raise<JobError>)
+    fun findById(id: JobId): Job
+}
+
+class LiveJobs : Jobs {
+    override fun Raise<JobError>.getById(id: JobId): Job =
+        JOBS_DATABASE[id] ?: raise(JobNotFound(id))
+
+    context(Logger, Raise<JobError>)
+    override fun findById(id: JobId): Job {
+//        info("Retrieving job with id $id")
+        return JOBS_DATABASE[id] ?: raise(JobNotFound(id))
+    }
+}
+
+interface JsonScope<T> {
+    fun T.toJson(): String
+}
+
+interface Logger {
+    fun info(message: String)
+}
+
+val consoleLogger = object : Logger {
+    override fun info(message: String) {
+        println("[INFO] $message")
+    }
+}
+
+sealed interface JobError
+data class JobNotFound(val jobId: JobId) : JobError
+data class GenericError(val cause: String) : JobError
+data object NegativeSalary : JobError
+
+data class Job(val id: JobId, val company: Company, val role: Role, val salary: Salary)
+
+@JvmInline
+value class JobId(val value: Long)
+
+@JvmInline
+value class Company(val name: String)
+
+@JvmInline
+value class Role(val name: String)
+
+@JvmInline
+value class Salary(val value: Double) {
+    operator fun compareTo(other: Salary): Int = value.compareTo(other.value)
+}
+
+val JOBS_DATABASE: Map<JobId, Job> = mapOf(
+    JobId(1) to Job(
+        JobId(1),
+        Company("Apple, Inc."),
+        Role("Software Engineer"),
+        Salary(70_000.00),
+    ),
+    JobId(2) to Job(
+        JobId(2),
+        Company("Microsoft"),
+        Role("Software Engineer"),
+        Salary(80_000.00),
+    ),
+    JobId(3) to Job(
+        JobId(3),
+        Company("Google"),
+        Role("Software Engineer"),
+        Salary(90_000.00),
+    ),
+)
+
+fun Raise<JobNotFound>.appleJob(): Job = JOBS_DATABASE[JobId(1)]!!
+fun Raise<JobNotFound>.jobNotFound(): Job = raise(JobNotFound(JobId(42)))
+
+
+// Arrow Kt Library
+public interface Raise<in Error> {
+    @RaiseDSL
+    public fun raise(r: Error): Nothing
+    // Omissis
+}
+
+
+class JobsService(private val jobs: Jobs) {
+
+    fun printSalary(jobId: JobId) = with (consoleLogger) {
+        fold(
+            block = { jobs.findById(jobId) },
+            recover = { error: JobError ->
+                when (error) {
+                    is JobNotFound -> println("Job with id ${jobId.value} not found")
+                    else -> println("An error was raised: $error")
+                }
+            },
+            transform = TODO(),
+            catch = (Throwable::printStackTrace)
+        )
+    }
+}
+
+
+
+fun main() {
+    val appleJobId = JobId(1)
+    val jobs = LiveJobs()
+    val jobService = JobsService(jobs)
+    jobService.printSalary(appleJobId)
+}
